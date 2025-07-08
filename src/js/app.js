45b14a527f9c6047e717d5b2b9aa61f16cc76866
PRODY (Juanma) <69425794+Juanma-venom@users.noreply.github.com>
2025-07-08 16:51:37 +0000

    Contidional Profile Card

--- src/js/app.js
+++ src/js/app.js
@@ -44,7 +44,7 @@ function render(variables = {}) {
       ? "Your lastname"
       : variables.lastName
   }</h1>
-          <h2${
+          <h2>${
             !variables.role || variables.role === "null"
               ? "Web Developer"
               : variables.role
@@ -59,13 +59,7 @@ function render(variables = {}) {
       : variables.country
   }</h3>
 
-          <ul class="position-left">
-          ${
-            !variables.socialMediaPosition ||
-            variables.socialMediaPosition === "position-left"
-              ? "position-right"
-              : variables.socialMediaPosition
-          }
+          <ul class="position-left" || ? === "position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
