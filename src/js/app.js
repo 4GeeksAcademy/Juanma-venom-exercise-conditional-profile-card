002717b98b0c85253671c24a6805ad55be761faa
PRODY (Juanma) <69425794+Juanma-venom@users.noreply.github.com>
2025-07-08 17:14:46 +0000

    Contidional Profile Card

--- src/js/app.js
+++ src/js/app.js
@@ -65,10 +65,18 @@ function render(variables = {}) {
               ? "position-right"
               : variables.socialMediaPosition
           }>
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${
+              variables.twitter
+            }" target="_blank"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github
+            }" target="_blank"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/school/${
+              variables.linkedin
+            }" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram
+            }" target="_blank"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
