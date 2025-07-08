f69d7b19b27dac4673d89b974b6b8f007392348c
PRODY (Juanma) <69425794+Juanma-venom@users.noreply.github.com>
2025-07-08 15:42:29 +0000

    Contidional Profile Card

--- src/js/app.js
+++ src/js/app.js
@@ -58,7 +58,8 @@ function render(variables = {}) {
       ? "USA"
       : variables.country
   }</h3>
-          <ul class="position-right">
+          <ul class="position-left">
+          ${!variables.socialMediaPosition}
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
